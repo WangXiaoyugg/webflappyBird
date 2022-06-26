968ca3eafbb7bb9cdd178667fed5aff34e2afc1a
GarenWang <2270099149@qq.com>
2022-06-26 14:37:15 +0800

    feat: 开发计分器类

--- src/js/player/Score.js
+++ src/js/player/Score.js
@@ -1,1 +1,21 @@
-export class Score {}
+// 计分器类
+import { DataStore } from '../base/DataStore.js'
+export class Score {
+  constructor() {
+    this.ctx = DataStore.getInstance().ctx
+    this.scoreNumber = 0
+    // canvas 的刷新频率很快，需要一个标志位控制是否可以加分
+    this.isScore = true
+  }
+
+  draw() {
+    this.ctx.font = '25px Arial'
+    this.ctx.fillStyle = '#ffcbca'
+    this.ctx.fillText(
+      this.scoreNumber,
+      window.innerWidth / 2,
+      window.innerHeight / 18,
+      1000
+    )
+  }
+}
